ed9f6bea1e1312e06c44f9682c300e3db826af19
NileshAm <nileshamarathunge@gmail.com>
2025-06-25 23:44:17 +0530

    Image fetch fixed

--- app/utils/imageFetch.ts
+++ app/utils/imageFetch.ts
@@ -1,3 +1,3 @@
-export function CDNFetch(ImageName:string): string {
-    return `https://cdn.jsdelivr.net/gh/NileshAm/Portfolio/public/images/${ImageName}`;
+export function CDNFetch(ImageName: string): string {
+  return `https://cdn.jsdelivr.net/gh/NileshAm/Portfolio@latest/public/images/${ImageName}`;
 }
