d912e5bde44a5a3fd1a15de8e83de810813d5237
NileshAm <nileshamarathunge@gmail.com>
2025-07-05 13:48:22 +0530

    margins and padding adjusted on /about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 import { CDNFetch } from "@utils/imageFetch";
 const page = () => {
   return (
-    <div className="m-4">
+    <main className="m-4 mx-5">
       <div className="flex">
         <div className="w-5/6">
           <h1 className="text-4xl text-highlight">About Me</h1>
-          <p className="text-lg mt-2 text-justify">
+          <p className="text-lg mt-2 text-justify px-5 leading-relaxed">
             Hello! I&apos;m{" "}
             <span className="text-highlight">A.M. Nilesh Amarathunge</span>, a
             tech enthusiast and engineering undergraduate with a strong passion
@@ -16,7 +16,7 @@ const page = () => {
           </p>
           <hr className="my-6 " />
           <h2 className="text-3xl text-highlight ">My Academic Journey</h2>{" "}
-          <p className="text-justify mt-2">
+          <p className="text-justify mt-2 px-5 leading-relaxed">
             My educational foundation was laid at Maliyadeva College,
             Kurunegala, where I actively balanced academics with innovation and
             extracurricular leadership. I sat for the GCE Ordinary Level
@@ -48,23 +48,23 @@ const page = () => {
       <h2 className="text-3xl text-highlight">
         Technical Projects and Experience
       </h2>{" "}
-      <p className="text-justify mt-2">
+      <p className="text-justify mt-2 mx-5 leading-relaxed">
         I&apos;ve had the opportunity to work on diverse technical projects,
         combining both hardware and software development.
       </p>
-      <ul className="list-disc ms-8 mt-2">
-        <li className="mt-2">
+      <ul className="list-disc ms-14 mt-2 leading-relaxed text-justify">
+        <li className="mt-4">
           Robogames (Team LYNX): Designed and fabricated the 3D CAD model of a
           battle robot and supported component integration. Our team competed
           successfully, winning 2 matches.
         </li>
-        <li className="mt-2">
+        <li className="mt-4">
           ThermoBand EN1190: A wearable health-monitoring device. I led the 3D
           design, component selection (ESP8266 + battery management), custom PCB
           co-design, firmware development, and backend integration with
           Express.js and Supabase.
         </li>
-        <li className="mt-2">
+        <li className="mt-4">
           D-Mobile E-Commerce App: Designed and developed a full-stack web
           application for an electronics store, using ReactJS for the frontend,
           Express/Node.js for the backend, and MySQL for a relational database.
@@ -73,14 +73,14 @@ const page = () => {
       <hr className="my-6" />
       <h2 className="text-3xl text-highlight">Awards & Recognition</h2>
       <ul className="list-disc ms-8 mt-2">
-        <li className="mt-2">
+        <li className="mt-4">
           Robotic Minds Competition 2015 — 1st Place (National level)
         </li>
-        <li className="mt-2">
+        <li className="mt-4">
           National Level Invention Competitions (2014-2015) — 1st, 2nd, and 3rd
           places at provincial and national level (“Sahasak Nimavum” by SLIC)
         </li>
-        <li className="mt-2">
+        <li className="mt-4">
           National Stamp Exhibition 2017 — 1st Place (organized by the
           Philatelic Bureau)
         </li>
@@ -90,24 +90,24 @@ const page = () => {
         Leadership & Extracurricular Activities
       </h2>
       <ul className="list-disc ms-8 mt-2">
-        <li className="mt-2">
+        <li className="mt-4">
           Vice Secretary, Philatelic Society - Maliyadeva College
         </li>
-        <li className="mt-2">
+        <li className="mt-4">
           Treasurer, Inventors&apos; Club - Maliyadeva College
         </li>
-        <li className="mt-2">
+        <li className="mt-4">
           Scout (2012-2020) - Committed member of the Maliyadeva Scout Troop,
           building resilience and leadership
         </li>
-        <li className="mt-2">
+        <li className="mt-4">
           Board Member, Philatelic & Inventors&apos; Societies - Nurtured my
           interest in creativity, design, and innovation
         </li>
       </ul>
       <hr className="my-6" />
       <h2 className="text-3xl text-highlight">Looking Ahead</h2>
-      <p className="text-justify mt-2">
+      <p className="text-justify mt-2 leading-loose px-5">
         I&apos;m deeply committed to bridging software and hardware through
         projects that improve lives and push technical boundaries. My goal is to
         become a multidisciplinary engineer, leveraging embedded systems,
@@ -115,7 +115,7 @@ const page = () => {
         Whether it&apos;s robotics, IoT, or smart wearables, I&apos;m excited to
         keep learning, building, and collaborating with like-minded innovators.
       </p>
-    </div>
+    </main>
   );
 };
 
